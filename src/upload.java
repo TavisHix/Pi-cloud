89a0eb972968acba5ef2b4da20e6badcc6252939
Tavis Hicks <thicks1207@gmail.com>
2019-10-21 00:36:48 -0400

    intial commit

--- src/upload.java
+++ src/upload.java
@@ -13,7 +13,7 @@ public class upload {
     public static void main(String[] args) {
     	org.apache.tools.ant.taskdefs.optional.ssh.Scp scp = new Scp();
     	int portSSH = 80;
-    	String srvrSSH = "47.37.224.79";
+    	String srvrSSH = "";
     	String userSSH = "pi"; 
     	String pswdSSH = "project19";
     	String localFile = "C:/Users/Tavis' Laptop/Desktop/john.txt";
